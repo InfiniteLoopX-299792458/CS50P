97b13a4dfb3c1cf04762ebfbbd59e46e5f64cf18
InfiniteLoopX-299792458 <gunaavula8@gmail.com>
2026-06-19 20:38:30 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -9,4 +9,6 @@ name = input("What's your name? ")
 print("hello,", end="")
 print(name)
 
-print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+# print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+
+print('hello, \'friend\'')
