408e7a194833d97b68de982cbe66bd44f01661fe
InfiniteLoopX-299792458 <gunaavula8@gmail.com>
2026-06-19 20:43:30 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -12,3 +12,14 @@ print(name)
 # print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
 
 print('hello, \'friend\'')
+
+# Ask the user for their name
+name = input("What's your name? ")
+
+# Ask the user for their name
+name = input("What's your name? ")
+print(f"hello, {name}")
+
+# Notice the f in print(f"hello, {name}").
+# This f is a special indicator for Python to treat this string a special way
+# This f string is a more often used these days.
