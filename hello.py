8b803fa81a884eb685fde9f1a312d076aa255a62
InfiniteLoopX-299792458 <gunaavula8@gmail.com>
2026-06-26 00:37:08 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -26,6 +26,9 @@ print(f"hello, {name}")
 # Notice the f in print(f"hello, {name}").
 # This f is a special indicator for Python to treat this string a special way
 # This f string is a more often used these days.
+# Think of it like this:
+# "..." → normal string
+# f"..." → string with live variables/calculations inside {}
 
 # 👇🏻 By utilizing the strip method on name (for example, name = name.strip()), you will remove any whitespace from the left and right of the user’s input. You can modify your code to be:
 # Ask the user for their name
@@ -38,7 +41,7 @@ name = name.strip()
 print(f"hello, {name}")
 
 
-#👇🏻Using the title method, it would title case the user’s name:
+#👇🏻Using the title method, it would title case the user’s name: [even if u enter it in lowercase, will automatically capitalize the first letter of each word]
 # Ask the user for their name
 name = input("What's your name? ")
 
@@ -47,6 +50,22 @@ name = name.strip()
 
 # Capitalize the first letter of each word
 name = name.title()
+
+# Print the output
+print(f"hello, {name}")
+
+
+#👇🏻More efficient:
+name = input("What's your name? ").strip().title()
+print(f"hello, {name}")
+
+#or
+
+# Ask the user for their name
+name = input("What's your name? ")
+
+# Remove whitespace from the str and capitalize the first letter of each word
+name = name.strip().title()
 
 # Print the output
 print(f"hello, {name}")
