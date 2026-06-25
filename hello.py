d156d09d2d1d7d6364968e8e7b4075524687776b
InfiniteLoopX-299792458 <gunaavula8@gmail.com>
2026-06-25 20:59:33 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -16,6 +16,9 @@ print('hello, \'friend\'')
 # Ask the user for their name
 name = input("What's your name? ")
 
+# Remove whitespace from the str (to remove the extra spaces that is entered by mistake)
+name = name.strip()
+
 # Ask the user for their name
 name = input("What's your name? ")
 print(f"hello, {name}")
@@ -23,3 +26,13 @@ print(f"hello, {name}")
 # Notice the f in print(f"hello, {name}").
 # This f is a special indicator for Python to treat this string a special way
 # This f string is a more often used these days.
+
+# 👇🏻 By utilizing the strip method on name (for example, name = name.strip()), you will remove any whitespace from the left and right of the user’s input. You can modify your code to be:
+# Ask the user for their name
+name = input("What's your name? ")
+
+# Remove whitespace from the str
+name = name.strip()
+
+# Print the output
+print(f"hello, {name}")
