1075bc7d0739bb381d6d0e71742549b4114cdad0
InfiniteLoopX-299792458 <gunaavula8@gmail.com>
2026-06-26 20:26:55 +0530

    Update 2.code calculator.py

--- 2.code calculator.py
+++ 2.code calculator.py
@@ -18,3 +18,8 @@ z = int(x) + int(y)
 print(z)
 #👆🏻The result is now correct. 
 #👆🏻The use of int(x) is called “casting,” where a value is temporarily changed from one type of variable (in this case, a string) to another (here, an integer).
+
+#👇🏻We can further improve our program as follows:
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+print(x + y)
