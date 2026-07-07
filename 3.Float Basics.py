91d78da88836c39edf75de0165d52c143fce5c40
InfiniteLoopX-299792458 <gunaavula8@gmail.com>
2026-07-07 22:34:00 +0530

    Update 3.Float Basics.py

---
+++ 3.Float Basics.py
@@ -0,0 +1,17 @@
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+print(x + y)
+
+#👇🏻The output will be rounded to the nearest integer.
+# Get the user's input
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+# Create a rounded result
+z = round(x + y)
+
+# Print the result
+print(z)
+
+#👇🏻
