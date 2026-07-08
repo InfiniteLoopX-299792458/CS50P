6049a1c5250a5e98e2ebdfd987c9fa1456ee849e
InfiniteLoopX-299792458 <gunaavula8@gmail.com>
2026-07-08 16:47:01 +0530

    Create 4.Def.py

---
+++ 4.Def.py
@@ -0,0 +1,7 @@
+#Wouldn’t it be nice to create our own functions?
+
+#
+# Ask the user for their name, remove whitespace from the str and capitalize the first letter of each word
+name = input("What's your name? ").strip().title()
+# Print the output
+print(f"hello, {name}")
