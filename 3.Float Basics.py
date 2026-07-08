75c0fa6cf40c360d7ef02d5e12db397923f456d7
InfiniteLoopX-299792458 <gunaavula8@gmail.com>
2026-07-08 16:47:06 +0530

    Update 3.Float Basics.py

--- 3.Float Basics.py
+++ 3.Float Basics.py
@@ -21,3 +21,20 @@ z = round(x + y)
 # Print the formatted result
 print(f"{z:,}")
 
+#👇🏻As we might expect, this will round the result to the nearest two decimal points.
+# Get the user's input
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+# Calculate the result and round
+z = round(x / y, 2)
+# Print the result
+print(z)
+
+#👇🏻This cryptic f-string code displays the same as our prior rounding strategy.
+# Get the user's input
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+# Calculate the result
+z = x / y
+# Print the result
+print(f"{z:.2f}")
