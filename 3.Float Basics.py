47257a4228b2bdff3ee7d89de9f27e56c7afa7a9
InfiniteLoopX-299792458 <gunaavula8@gmail.com>
2026-07-08 14:08:55 +0530

    Update 3.Float Basics.py

--- 3.Float Basics.py
+++ 3.Float Basics.py
@@ -7,11 +7,17 @@ print(x + y)
 # Get the user's input
 x = float(input("What's x? "))
 y = float(input("What's y? "))
-
 # Create a rounded result
 z = round(x + y)
-
 # Print the result
 print(z)
 
-#👇🏻
+#👇🏻Though quite cryptic, that print(f"{z:,}") creates a scenario where the outputted z will include commas where the result could look like 1,000 or 2,500.
+# Get the user's input
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+# Create a rounded result
+z = round(x + y)
+# Print the formatted result
+print(f"{z:,}")
+
