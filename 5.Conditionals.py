e7fcb7438a5534124f58fb30b44f27c48d15167b
InfiniteLoopX-299792458 <gunaavula8@gmail.com>
2026-07-09 20:13:36 +0530

    Create 5.Conditionals.py

---
+++ 5.Conditionals.py
@@ -0,0 +1,21 @@
+# >= denotes “greater than or equal to.”
+# <= denotes “less than or equal to.”
+# == denotes “equals.” Note the double equal sign: a single equal sign assigns a value, whereas two equal signs compare values.
+# != denotes “not equal to.”
+
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+if x < y:
+    print("x is less than y")
+
+
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+if x < y:
+    print("x is less than y")
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x is equal to y")
+
+
