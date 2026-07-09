63e83f0408405abfcea7170730c5ee7125b93129
InfiniteLoopX-299792458 <gunaavula8@gmail.com>
2026-07-09 22:10:41 +0530

    Update 5.Conditionals.py

--- 5.Conditionals.py
+++ 5.Conditionals.py
@@ -194,3 +194,40 @@ else:
 # =============================================================================
 
 #👇🏻Similar to or, and can be used within conditional statements.
+score = int(input("Score: "))
+if score >= 90 and score <= 100:
+    print("Grade: A")
+elif score >=80 and score < 90:
+    print("Grade: B")
+elif score >=70 and score < 80:
+    print("Grade: C")
+elif score >=60 and score < 70:
+    print("Grade: D")
+else:
+    print("Grade: F")
+
+#👇🏻We could improve our code as follows:
+ score = int(input("Score: "))
+  if 90 <= score <= 100:
+      print("Grade: A")
+  elif 80 <= score < 90:
+      print("Grade: B")
+  elif 70 <= score < 80:
+      print("Grade: C")
+  elif 60 <= score < 70:
+      print("Grade: D")
+  else:
+      print("Grade: F")
+    
+#👇🏻Can you improve the code further?, yes, like this:
+score = int(input("Score: "))
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
