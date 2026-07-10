8517827ca8cf604d50b978add7a151a609fc81c9
InfiniteLoopX-299792458 <gunaavula8@gmail.com>
2026-07-10 13:56:09 +0530

    Update 5.Conditionals.py

--- 5.Conditionals.py
+++ 5.Conditionals.py
@@ -231,3 +231,11 @@ elif score >= 60:
     print("Grade: D")
 else:
     print("Grade: F")
+
+#👇🏻The modulo "%" operator in programming allows one to see if two numbers divide evenly or divide and have a remainder.
+#For example, 4 % 2 would result in zero, because it evenly divides. However, 3 % 2 does not divide evenly and would result in a number other than zero!
+x = int(input("What's x? "))
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
